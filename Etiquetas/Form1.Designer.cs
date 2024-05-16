62908e4a1a08388810f0cf435964ff331b504426
Henrique Lustosa Ribeiro Faria <h013026@rede.sp>
2024-05-16 16:41:35 -0300

    Etiqueta nova versao conforme solicitacao da Alergia

--- Etiquetas/Form1.Designer.cs
+++ Etiquetas/Form1.Designer.cs
@@ -36,13 +36,25 @@
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.lblError = new System.Windows.Forms.Label();
             this.lbTitulo = new System.Windows.Forms.Label();
+            this.cbxBacteria = new System.Windows.Forms.CheckBox();
+            this.cbxInsetos = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbVolume = new System.Windows.Forms.TextBox();
+            this.txbLote = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbFabricacao = new System.Windows.Forms.MaskedTextBox();
+            this.txbValidade = new System.Windows.Forms.MaskedTextBox();
+            this.txbDiluicao = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btImprimir
             // 
-            this.btImprimir.Location = new System.Drawing.Point(192, 196);
+            this.btImprimir.Location = new System.Drawing.Point(124, 456);
             this.btImprimir.Name = "btImprimir";
-            this.btImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btImprimir.Size = new System.Drawing.Size(116, 34);
             this.btImprimir.TabIndex = 5;
             this.btImprimir.Text = "Imprimir";
             this.btImprimir.UseVisualStyleBackColor = true;
@@ -53,9 +65,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(23, 81);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.Size = new System.Drawing.Size(23, 13);
             this.label1.TabIndex = 11;
-            this.label1.Text = "Paciente";
+            this.label1.Text = "RH";
             // 
             // txbRh
             // 
@@ -95,11 +107,126 @@
             this.lbTitulo.TabIndex = 0;
             this.lbTitulo.Text = "Aplicação Etiqueta Alergia";
             // 
+            // cbxBacteria
+            // 
+            this.cbxBacteria.AutoSize = true;
+            this.cbxBacteria.Location = new System.Drawing.Point(26, 121);
+            this.cbxBacteria.Name = "cbxBacteria";
+            this.cbxBacteria.Size = new System.Drawing.Size(234, 17);
+            this.cbxBacteria.TabIndex = 12;
+            this.cbxBacteria.Text = "D. pteromyssinus + B. Tropicalis + Ext. Bact.";
+            this.cbxBacteria.UseVisualStyleBackColor = true;
+            // 
+            // cbxInsetos
+            // 
+            this.cbxInsetos.AutoSize = true;
+            this.cbxInsetos.Location = new System.Drawing.Point(26, 152);
+            this.cbxInsetos.Name = "cbxInsetos";
+            this.cbxInsetos.Size = new System.Drawing.Size(60, 17);
+            this.cbxInsetos.TabIndex = 13;
+            this.cbxInsetos.Text = "Insetos";
+            this.cbxInsetos.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 251);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Volume";
+            // 
+            // txbVolume
+            // 
+            this.txbVolume.Location = new System.Drawing.Point(124, 243);
+            this.txbVolume.Name = "txbVolume";
+            this.txbVolume.Size = new System.Drawing.Size(100, 20);
+            this.txbVolume.TabIndex = 15;
+            // 
+            // txbLote
+            // 
+            this.txbLote.Location = new System.Drawing.Point(124, 301);
+            this.txbLote.Name = "txbLote";
+            this.txbLote.Size = new System.Drawing.Size(100, 20);
+            this.txbLote.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 309);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Lote";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 358);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Fabricacao";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 400);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Validade";
+            // 
+            // txbFabricacao
+            // 
+            this.txbFabricacao.Location = new System.Drawing.Point(124, 358);
+            this.txbFabricacao.Mask = "00/00/0000";
+            this.txbFabricacao.Name = "txbFabricacao";
+            this.txbFabricacao.Size = new System.Drawing.Size(75, 20);
+            this.txbFabricacao.TabIndex = 21;
+            // 
+            // txbValidade
+            // 
+            this.txbValidade.Location = new System.Drawing.Point(124, 400);
+            this.txbValidade.Mask = "00/00/0000";
+            this.txbValidade.Name = "txbValidade";
+            this.txbValidade.Size = new System.Drawing.Size(75, 20);
+            this.txbValidade.TabIndex = 22;
+            // 
+            // txbDiluicao
+            // 
+            this.txbDiluicao.Location = new System.Drawing.Point(124, 199);
+            this.txbDiluicao.Name = "txbDiluicao";
+            this.txbDiluicao.Size = new System.Drawing.Size(100, 20);
+            this.txbDiluicao.TabIndex = 24;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Diluição";
+            // 
             // Form1
             // 
+            this.AcceptButton = this.btImprimir;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 269);
+            this.ClientSize = new System.Drawing.Size(387, 539);
+            this.Controls.Add(this.txbDiluicao);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txbValidade);
+            this.Controls.Add(this.txbFabricacao);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txbLote);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbVolume);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxInsetos);
+            this.Controls.Add(this.cbxBacteria);
             this.Controls.Add(this.lbTitulo);
             this.Controls.Add(this.lblError);
             this.Controls.Add(this.label1);
@@ -122,6 +249,18 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Label lblError;
         private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.CheckBox cbxBacteria;
+        private System.Windows.Forms.CheckBox cbxInsetos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbVolume;
+        private System.Windows.Forms.TextBox txbLote;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox txbFabricacao;
+        private System.Windows.Forms.MaskedTextBox txbValidade;
+        private System.Windows.Forms.TextBox txbDiluicao;
+        private System.Windows.Forms.Label label6;
 
     }
 }
--- Etiquetas/Form1.cs
+++ Etiquetas/Form1.cs
@@ -7,12 +7,15 @@ using System.Net;
 using System.IO;
 using System.Drawing.Imaging;
 using System.Drawing.Drawing2D;
+using Newtonsoft.Json;
 
 namespace Etiquetas
 {
     public partial class Form1 : Form
     {
+        Paciente paciente;
         String error;
+        private const int DesiredLength = 12; // Desired length of the text
 
         int status;
   
@@ -23,7 +26,21 @@ namespace Etiquetas
             error = "";
             printDocument1.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Custom2", 197, 118);
             printDialog1.PrinterSettings.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Custom2", 197, 118);
-            
+
+            this.txbRh.Focus();
+            // Set TabIndex
+            txbRh.TabIndex = 0;
+            cbxBacteria.TabIndex = 1;
+            cbxInsetos.TabIndex = 2;
+            txbDiluicao.TabIndex = 3;
+            txbVolume.TabIndex = 4;
+            txbLote.TabIndex = 5;
+            txbFabricacao.TabIndex = 6;
+            txbValidade.TabIndex = 7;
+            btImprimir.TabIndex = 8;
+
+
+
         }
       
         private void btImprimir_Click(object sender, EventArgs e)
@@ -32,7 +49,7 @@ namespace Etiquetas
             backgroundWorker1.RunWorkerAsync();
             this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
             //btImprimir.Enabled = true;
-
+            
         }
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
@@ -44,8 +61,10 @@ namespace Etiquetas
             this.txbRh.ResetText();
             this.txbRh.Enabled = true;
             this.txbRh.Focus();
-            this.txbRh.Text = "";
-         
+          
+            ClearAllTextBoxes(this);
+            ClearAllCheckBoxes(this);
+            ClearAllMaskedTextBoxes(this);
         }
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
@@ -82,16 +101,55 @@ namespace Etiquetas
 
                 }
             }
-        
-     
+
+        public class Paciente
+        {
+            
+            public string nm_nome { get; set; }
+            
+
+
+        }
 
         private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
         {
             DateTime data = DateTime.Now;
-            
+            string rh = txbRh.Text;
+            //detiq = dados.getDados(be);
+            string text = txbVolume.Text;
+            if (text.Length < DesiredLength)
+            {
+                text = text.PadRight(DesiredLength); // Pad the text with spaces
+            }
+            else if (text.Length > DesiredLength)
+            {
+                text = text.Substring(0, DesiredLength); // Truncate the text to the desired length
+            }
+            string url = "http://10.48.21.64:5003/hspmsgh-api/pacientes/paciente/" + rh;
+            WebRequest request = WebRequest.Create(url);
+            try
+            {
+                using (var twitpicResponse = (HttpWebResponse)request.GetResponse())
+                {
+                    using (var reader = new StreamReader(twitpicResponse.GetResponseStream()))
+                    {
+                        JsonSerializer json = new JsonSerializer();
+                        var objText = reader.ReadToEnd();
+                        paciente = JsonConvert.DeserializeObject<Paciente>(objText);
+
+                    }
+                }
+
+                string[] nameParts = paciente.nm_nome.Split(' ');
+                for (int i = 1; i < nameParts.Length - 1; i++)
+                {
+                    nameParts[i] = nameParts[i][0] + ".";
+                }
+                // Join the parts back together
+                string abbreviatedName = string.Join(" ", nameParts);
                 e.PageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Custom2", 197, 118);//900 é a largura da página
                 printDocument1.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Custom2", 197, 118);
-                printDialog1.PrinterSettings.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Custom2",197, 118);
+                printDialog1.PrinterSettings.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Custom2", 197, 118);
                 using (Graphics g = e.Graphics)
                 {
                     using (Font fnt = new Font("Arial", 12))
@@ -100,7 +158,7 @@ namespace Etiquetas
                         int startXEsquerda = 2;
                         int starty = 2;//distancia das linhas
                         // Create two images.
-                       
+
                         Image image1 = Image.FromFile("./Files/HSPM_LOGO.jpg");
                         var newImage = ResizeImage(image1, 35, 20);
                         // Get a PropertyItem from image1.
@@ -111,23 +169,23 @@ namespace Etiquetas
 
                         // Set the PropertyItem for image2.
                         //image1.SetPropertyItem(propItem);
-                       // RectangleF srcRect = new RectangleF(100.0F, 100.0F, 100.0F, 100.0F);
+                        // RectangleF srcRect = new RectangleF(100.0F, 100.0F, 100.0F, 100.0F);
                         // GraphicsUnit units = GraphicsUnit.Millimeter;
                         // Draw the image.
                         e.Graphics.DrawImage(newImage, 5.0F, 0.0F);
 
 
 
-                         // Create image.
-                       // Image newImage = Image.FromFile("C://Users/h013026/Downloads/HSPM_LOGO.jpg");
-             
+                        // Create image.
+                        // Image newImage = Image.FromFile("C://Users/h013026/Downloads/HSPM_LOGO.jpg");
+
                         // Create coordinates for upper-left corner of image.
-                       // float x = 0.0F;
-                       /// float y = 0.0F;
-                                 
+                        // float x = 0.0F;
+                        /// float y = 0.0F;
+
                         // Create rectangle for source image.
-                       // RectangleF srcRect = new RectangleF(100.0F, 100.0F, 100.0F, 100.0F);
-                       // GraphicsUnit units = GraphicsUnit.Pixel;
+                        // RectangleF srcRect = new RectangleF(100.0F, 100.0F, 100.0F, 100.0F);
+                        // GraphicsUnit units = GraphicsUnit.Pixel;
                         //         
                         // Draw image to screen.
                         //e.Graphics.DrawImage(newImage, x, y, srcRect, units);
@@ -135,10 +193,10 @@ namespace Etiquetas
 
                         //g.DrawImage(image1, new Rectangle(10, 10, 200, 200));
 
-                    
 
-                                       
-                       //newImage.Save("c:\\newImage.png", ImageFormat.Png);
+
+
+                        //newImage.Save("c:\\newImage.png", ImageFormat.Png);
 
                         Pen blackPen = new Pen(Color.Black, 1);
 
@@ -147,13 +205,23 @@ namespace Etiquetas
                         int y = starty + 40;
                         int width = 7;
                         int height = 7;
+                        if (cbxBacteria.Checked)
+                        {
+                            // Draw a filled black rectangle
+                            g.FillRectangle(Brushes.Black, x, y, width, height);
+                        }
+                        else
+                        {
+                            // Draw an outlined rectangle
+                            g.DrawRectangle(blackPen, x, y, width, height); ;
+                        }
 
                         // Draw rectangle to screen.
-                        e.Graphics.DrawRectangle(blackPen, x, y, width, height);
+                       // e.Graphics.DrawRectangle(blackPen, x, y, width, height);
 
 
-                        g.DrawString("Clínica de Alergia" , new Font("Sans Serif", 8, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda + 45, starty );
-                        g.DrawString("Paciente: " + txbRh.Text, new Font("Sans Serif", 7, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda , starty + 24);
+                        g.DrawString("Clínica de Alergia", new Font("Sans Serif", 8, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda + 45, starty);
+                        g.DrawString("Paciente: " + abbreviatedName, new Font("Sans Serif", 7, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda, starty + 24);
 
 
                         g.DrawString("D. pteromyssinus + B. Tropicalis + Ext. Bact.", new Font("Sans Serif", 5, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda + 10, starty + 40);
@@ -163,24 +231,83 @@ namespace Etiquetas
                         y = starty + 56;
                         width = 7;
                         height = 7;
-
+                        if (cbxInsetos.Checked)
+                        {
+                            // Draw a filled black rectangle
+                            g.FillRectangle(Brushes.Black, x, y, width, height);
+                        }
+                        else
+                        {
+                            // Draw an outlined rectangle
+                            g.DrawRectangle(blackPen, x, y, width, height); ;
+                        }
                         // Draw rectangle to screen.
-                        e.Graphics.DrawRectangle(blackPen, x, y, width, height);
-                        g.DrawString("Insetos            " + "Diluição:........................", new Font("Sans Serif", 6, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda + 10, starty + 56);
-                        g.DrawString("Volume: 7,0             " + "Lote:........................", new Font("Sans Serif", 6, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda, starty + 72);
-                        g.DrawString("Fab.: ...../...../....." + "    Val.: ...../....../.....", new Font("Sans Serif", 7, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda, starty + 98);
+                       // e.Graphics.DrawRectangle(blackPen, x, y, width, height);
+                        g.DrawString("Insetos            " + "Diluição: " + txbDiluicao.Text, new Font("Sans Serif", 6, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda + 10, starty + 56);
+                        g.DrawString("Volume: " + text + "Lote: " + txbLote.Text, new Font("Sans Serif", 6, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda, starty + 72);
+                        g.DrawString("Fab.: " + txbFabricacao.Text+ "    Val.: " + txbValidade.Text, new Font("Sans Serif", 7, FontStyle.Bold), System.Drawing.Brushes.Black, startXEsquerda, starty + 98);
 
                         System.Drawing.StringFormat drawFormat = new System.Drawing.StringFormat();
                         drawFormat.FormatFlags = StringFormatFlags.DirectionVertical;
                         g.DrawString("Conservar 2º C - 8º C ", new Font("Sans Serif", 7, FontStyle.Bold), System.Drawing.Brushes.Black, 175, 5, drawFormat);
-                          
 
-                           
-                           
-                        }
+                        
+
+
                     }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Número de RH inexistente! " + ex.Message);
+                status = 1;
+
+            }
+        }
+        private void ClearAllTextBoxes(Control control)
+        {
+            foreach (Control c in control.Controls)
+            {
+                if (c is TextBox)
+                {
+                    ((TextBox)c).Text = string.Empty;
+                }
+                else
+                {
+                    ClearAllTextBoxes(c); // Recursively call for nested controls
                 }
 
+            }
+        }
+        private void ClearAllCheckBoxes(Control control)
+        {
+            foreach (Control c in control.Controls)
+            {
+                if (c is CheckBox)
+                {
+                    ((CheckBox)c).Checked = false;
+                }
+                else
+                {
+                    ClearAllCheckBoxes(c); // Recursively call for nested controls
+                }
+            }
+        }
+        private void ClearAllMaskedTextBoxes(Control control)
+        {
+            foreach (Control c in control.Controls)
+            {
+                if (c is MaskedTextBox)
+                {
+                    ((MaskedTextBox)c).Text = string.Empty;
+                }
+                else
+                {
+                    ClearAllMaskedTextBoxes(c); // Recursively call for nested controls
+                }
+            }
+        }
         /// <summary>
         /// Resize the image to the specified width and height.
         /// </summary>
@@ -224,6 +351,10 @@ namespace Etiquetas
 
             }
         }
+
+      
+
+      
 
      
     }
